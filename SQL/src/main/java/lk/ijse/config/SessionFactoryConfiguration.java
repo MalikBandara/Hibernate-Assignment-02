7b8232e8584afe3f2df2b04aae47fa20482a94f6
Your Name <you@example.com>
2024-09-17 10:14:30 +0530

    SQL Part Done

--- SQL/src/main/java/lk/ijse/config/SessionFactoryConfiguration.java
+++ SQL/src/main/java/lk/ijse/config/SessionFactoryConfiguration.java
@@ -1,6 +1,8 @@
 package lk.ijse.config;
 
 
+import lk.ijse.entity.Address;
+import lk.ijse.entity.Customer;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -12,7 +14,7 @@ public class SessionFactoryConfiguration {
     private SessionFactory session;
 
     private SessionFactoryConfiguration(){
-        Configuration configuration = new Configuration().configure();
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(Address.class).addAnnotatedClass(Customer.class);
         session = configuration.buildSessionFactory();
 
     }
